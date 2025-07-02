4c310b9b1a2052fd7a46f566556c39784af0c3f1
Thomas29836 <epargneplus9@gmail.com>
2025-07-02 09:58:42 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,3 +0,0 @@
-const supabaseUrl = 'https://sumuxiqjpctmplgbbxom.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1bXV4aXFqcGN0bXBsZ2JieG9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzODI5MTIsImV4cCI6MjA2Njk1ODkxMn0.cpafK-cveQ_NuTNNR6-2KmzpJlwJSwb5BFyEWSVndLA';
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
